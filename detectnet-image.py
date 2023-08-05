8905106a8026b141be2ab9f7a9aa85ba701d17cc
VietNguyenHoang812 <hoangviet1121999@gmail.com>
2023-08-05 07:04:42 +0700

    add: LPR engine and post-process for LPR

--- detectnet-image.py
+++ detectnet-image.py
@@ -15,7 +15,7 @@ import sys
 # parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
 # parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
 
-is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
+# is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
 
 # try:
 # 	opt = parser.parse_known_args()[0]
@@ -31,7 +31,7 @@ overlay = "box,labels,conf"
 # load the object detection network
 net = jetson_inference.detectNet(argv=["--model=networks/az_ocr/az_ocr_ssdmobilenetv1_2.onnx", "--labels=networks/az_ocr/labels.txt", "--input-blob=input_0", "--output-cvg=scores", "--output-bbox=boxes"], threshold=0.5)
 
-img = jetson_utils.loadImage("resized.jpg")
+img = jetson_utils.loadImage("cropped.jpg")
 
 # detect objects in the image (with overlay)
 detections = net.Detect(img, overlay=overlay)
@@ -39,9 +39,42 @@ detections = net.Detect(img, overlay=overlay)
 # print the detections
 print("detected {:d} objects in image".format(len(detections)))
 
+predictions = []
 for detection in detections:
-	print(detection)
+	# print(detection)
+	prediction = {
+		"Value": net.GetClassDesc(detection.ClassID),
+		"Left": detection.Left,
+		"Top": detection.Top,
+		"Sum": detection.Left + detection.Top
+	}
+	predictions.append(prediction)
+	# print(net.GetClassDesc(detection.ClassID))
+license_plate = ""
+sorted_list = sorted(predictions, key=lambda i: (i["Sum"], i["Top"]))
+if sorted_list[0]["Left"] > sorted_list[1]["Left"]:
+    first = sorted_list.pop(0)
+else:
+    first = sorted_list.pop(1)
 
+license_plate += first["Value"]
+while sorted_list:
+    top_first = first["Top"]
+    left_first = 9999
+    index_pop = -1
+    for i, p in enumerate(sorted_list):
+        top_p, left_p = p["Top"], p["Left"]
+        if top_p > 0.8*top_first and top_p < 1.2*top_first and left_p < left_first:
+            index_pop = i
+            left_first = left_p
+    if index_pop == -1:
+        sorted_list = sorted(sorted_list, key=lambda i: i["Sum"])
+        index_pop = 0
+
+    first = sorted_list.pop(index_pop)
+    license_plate += first["Value"]
+
+print(license_plate)
 # print out performance info
 net.PrintProfilerTimes()
 
