8219a1f54915fb77b1fd77debc1172eafc1a7d53
VietNguyenHoang812 <hoangviet1121999@gmail.com>
2023-09-26 22:18:46 +0700

    refactor: organize files and fix ocr_plate rules

--- LPR_jetson_nano/resize.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-
-ori_img = cv2.imread("cropped.jpg")
-resized = cv2.resize(ori_img, (640, 480))
-cv2.imwrite("resized.jpg", resized)
-# cv2.imshow("resized", resized)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
