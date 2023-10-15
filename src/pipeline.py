913f25628ff6fcd5b36c9b59ede8e515fddf3ff9
VietNguyenHoang812 <hoangviet1121999@gmail.com>
2023-10-15 20:52:13 +0700

    add: license plate ocr

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,13 @@
-from src.stages.abstract import LicensePlateDetection
+from src.stages.abstract import LicensePlateDetection, LicensePlateOCR, SpeedDetection
 
 
 if __name__ == "__main__":
+    # library = "src/yolov5/build/libmyplugins.so"
+    # engine = "src/yolov5/build/license_plate_detection.engine"
+    # source = "images/P1690088.JPG"
+    # LicensePlateDetection(library, engine, source).run()
+
     library = "src/yolov5/build/libmyplugins.so"
     engine = "src/yolov5/build/license_plate_detection.engine"
     source = "images/P1690088.JPG"
-    LicensePlateDetection(library, engine, source).run()
+    LicensePlateOCR(library, engine, source).run()
--- src/stages/abstract.py
+++ src/stages/abstract.py
@@ -64,10 +64,73 @@ class LicensePlateDetection(AbstractYoloTRT):
 
 
 class LicensePlateOCR(AbstractYoloTRT):
-    def __init__(self) -> None:
-        pass
+    def process(self):
+        detections, t, crops = self.model.Inference(self.image)
+        return (detections, t, crops)
+    
+    def postprocess(self, process_results, is_saved: bool = True):
+        detections, t, crops = process_results
+        if len(detections) == 0:
+            return None
+        
+        predictions = []
+        for i in range(len(detections)):  # per image
+            label_name, conf, box = detections[i]["class"], detections[i]["conf"], detections[i]["box"]
+            left, top = int(box[0]), int(box[1])
+            if label_name == "background":
+                continue
+            if conf < self.score_threshold:
+                continue
+            prediction = {
+                "Value": str(label_name),
+                "Score": conf,
+                "Top": top,
+                "Left": left,
+                "Sum": top+left,
+            }
+            predictions.append(prediction)
+        
+        # License plate must have at least 7 characters
+        if len(predictions) < 7:
+            return str(len(predictions))
+        
+        # Sort characters
+        license_plate = ""
+        HIGH_ALIGN_RATIO = 2
+        LOW_ALIGN_RATIO = 0.5
+        sorted_list = sorted(predictions, key=lambda i: (i["Sum"], i["Top"]))
+        align_ratio = sorted_list[0]["Top"]/sorted_list[1]["Top"]
+        if (align_ratio > HIGH_ALIGN_RATIO or align_ratio < LOW_ALIGN_RATIO) and sorted_list[1]["Left"] > sorted_list[0]["Left"]:
+            first = sorted_list.pop(1)
+        else:
+            first = sorted_list.pop(0)
 
+        license_plate += first["Value"]
+        while sorted_list:
+            top_first = first["Top"]
+            left_first = 9999
+            index_pop = -1
+            for i, p in enumerate(sorted_list):
+                top_p, left_p = p["Top"], p["Left"]
+                align_ratio = top_p/top_first
+                # check if p and first are on the same line
+                if align_ratio < HIGH_ALIGN_RATIO and align_ratio > LOW_ALIGN_RATIO and left_p < left_first:
+                    index_pop = i
+                    left_first = left_p
+            if index_pop == -1:
+                sorted_list = sorted(sorted_list, key=lambda i: i["Sum"])
+                index_pop = 0
 
+            first = sorted_list.pop(index_pop)
+            license_plate += first["Value"]
+        
+        return license_plate
+
+    def run(self):
+        process_results = self.process()
+        self.postprocess(process_results)
+
+        
 class SpeedDetection(AbstractYoloTRT):
     def __init__(self) -> None:
         pass
